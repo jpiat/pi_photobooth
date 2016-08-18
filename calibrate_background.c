ee871a0a94c4042df6cbef8174893067f5dd57ee
Jonathan Piat <piat.jonathan@gmail.com>
2016-08-18 14:43:00 +0200

    Moved bgr to hsv conversion in camera_config file

--- calibrate_background.c
+++ calibrate_background.c
@@ -25,63 +25,6 @@ RaspiCamCvCapture * capture;
 CvCapture * capture;
 #endif
 
-inline void bgr_to_hsv(char * bgr_pix, float * h, float * s, float * v) {
-
-#ifndef FAST_CONV
-	float r, g, b;
-	float c, M, m;
-	b = ((float) ((unsigned char*) bgr_pix)[0])/255.O;
-	g = ((float)  ((unsigned char*) bgr_pix)[1])/255.0;
-	r = ((float) ((unsigned char*) bgr_pix)[2])/255.0;
-	M = r > g ? r : g;
-	M = b > M ? b : M;
-	m = r < g ? r : g;
-	m = b < m ? b : m;
-	c = M - m;
-	(*h) = 0;
-	if (M == r)
-	(*h) = fmod(((g - b) / c), 6);
-	else if (M == g)
-	(*h) = ((b - r) / c) + 2;
-	else if (M == b)
-	(*h) = ((r - g) / c) + 4;
-	(*h) = (*h) * 60.0;
-	(*v) = M;
-	(*s) = (*v) > 0 ? (c / (*v)) : 0;
-
-#else
-	float r, g, b;
-	b = (float) ((unsigned char*) bgr_pix)[0];
-	g = (float) ((unsigned char*) bgr_pix)[1];
-	r = (float) ((unsigned char*) bgr_pix)[2];
-	float K = 0.f;
-	float chroma = 0.f;
-	if (g < b) {
-		float t;
-		t = g;
-		g = b;
-		b = t;
-		K = -1.f;
-	}
-
-	if (r < g) {
-		float t;
-		t = r;
-		r = g;
-		g = t;
-		K = -2.f / 6.f - K;
-	}
-
-	if (g > b)
-		chroma = r - b;
-	else
-		chroma = r - g;
-
-	(*h) = fabs(K + (g - b) / (6.f * chroma + 1e-20f));
-	(*s) = chroma / (r + 1e-20f);
-	(*v) = r;
-#endif
-}
 
 void learn_background(double * h_mean, double * s_mean, double * h_stdev,
 		double * s_stdev, int nb_images) {
--- pi_photobooth.c
+++ pi_photobooth.c
@@ -126,64 +126,6 @@ int timespec_subtract(struct timespec * result, struct timespec * x,
 	return x->tv_sec < y->tv_sec;
 }
 
-inline void bgr_to_hsv(char * bgr_pix, float * h, float * s, float * v) {
-
-#ifndef FAST_CONV
-	unsigned char r, g, b;
-	float c, M, m;
-	b = ((unsigned char*) bgr_pix)[0];
-	g = ((unsigned char*) bgr_pix)[1];
-	r = ((unsigned char*) bgr_pix)[2];
-	M = r > g ? r : g;
-	M = b > M ? b : M;
-	m = r < g ? r : g;
-	m = b < m ? b : m;
-	c = M - m;
-	(*h) = 0;
-	if (M == r)
-	(*h) = fmod(((g - b) / c), 6);
-	else if (M == g)
-	(*h) = ((b - r) / c) + 2;
-	else if (M == b)
-	(*h) = ((r - g) / c) + 4;
-	(*h) = (*h) * 60.0;
-	(*v) = M;
-	(*s) = (*v) > 0 ? (c / (*v)) : 0;
-
-#else
-	float r, g, b;
-	b = (float) ((unsigned char*) bgr_pix)[0];
-	g = (float) ((unsigned char*) bgr_pix)[1];
-	r = (float) ((unsigned char*) bgr_pix)[2];
-	float K = 0.f;
-	float chroma = 0.f;
-	if (g < b) {
-		float t;
-		t = g;
-		g = b;
-		b = t;
-		K = -1.f;
-	}
-
-	if (r < g) {
-		float t;
-		t = r;
-		r = g;
-		g = t;
-		K = -2.f / 6.f - K;
-	}
-
-	if (g > b)
-		chroma = r - b;
-	else
-		chroma = r - g;
-
-	(*h) = fabs(K + (g - b) / (6.f * chroma + 1e-20f));
-	(*s) = chroma / (r + 1e-20f);
-	(*v) = r;
-#endif
-}
-
 inline int erode_pos(IplImage * img, int x, int y) {
 	int eroded_pixel = 1;
 	eroded_pixel &= img->imageData[y * img->widthStep + (x * img->nChannels)];
