356356fdf2e31307a2e4cb51cdd4b30ff3318395
Jonathan Piat <piat.jonathan@gmail.com>
2016-08-24 07:43:46 +0000

    Changed psot-process script to run solely on the Pi

--- post_process_script.sh
+++ post_process_script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#sudo mount /dev/sda1 /home/pi/Pictures -o dmask=000,fmask=111
-BACKGROUND_FILES=${1}/background_photos/*
+sudo mount /dev/sda1 /home/pi/Pictures -o dmask=000,fmask=111
+BACKGROUND_FILES=/home/pi/Pictures/background_photos/*
 for fb in ${BACKGROUND_FILES}
 do
 	FILE_NAME=${fb%.*}
 	FILE_NAME=${FILE_NAME##*/}
 	echo ${FILE_NAME}
-	FOREGROUND_FILES=`find ${1}/foreground_photos -name "*_${FILE_NAME}.jpg"`
+	FOREGROUND_FILES=`find /home/pi/Pictures/foreground_photos -name "*_${FILE_NAME}.jpg"`
 	for fg in ${FOREGROUND_FILES}
 	do
-		FINISHED_NAME=${2}/${FILE_NAME}_finished.png
-		FINISHED_MASK=${2}/${FILE_NAME}_mask.png
+		FINISHED_NAME=${1}/${FILE_NAME}_finished.png
+		FINISHED_MASK=${1}/${FILE_NAME}_mask.png
 		convert ${fg} -bordercolor green -border 1x1 -matte -fill none -fuzz 15% -draw 'matte 1,1 floodfill' -shave 1x1 ${FINISHED_MASK}
 		composite -compose Dst_Over ${fb} ${FINISHED_MASK} ${FINISHED_NAME}
 	done
